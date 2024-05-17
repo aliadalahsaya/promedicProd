155193c1a128560ef2fe2cecee99e000182a4bf4
MangJali <yangsabaryangtaqwa@gmail.com>
2024-05-17 13:56:01 +0700

    update vercel.json

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,6 @@
 <!doctype html>
+<html class="no-js" lang="zxx">
+
 <head>
     <!-- Meta Tags -->
     <meta charset="utf-8">
@@ -985,43 +987,43 @@
     <!-- jquery Min JS -->
     <script src="public/js/jquery.min.js"></script>
     <!-- jquery Migrate JS -->
-    <script src="public/js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
     <!-- jquery Ui JS -->
-    <script src="public/js/jquery-ui.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
     <!-- Easing JS -->
-    <script src="public/js/easing.js"></script>
+    <script src="js/easing.js"></script>
     <!-- Color JS -->
-    <script src="public/js/colors.js"></script>
+    <script src="js/colors.js"></script>
     <!-- Popper JS -->
-    <script src="public/js/popper.min.js"></script>
+    <script src="js/popper.min.js"></script>
     <!-- Bootstrap Datepicker JS -->
-    <script src="public/js/bootstrap-datepicker.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
     <!-- Jquery Nav JS -->
-    <script src="public/js/jquery.nav.js"></script>
+    <script src="js/jquery.nav.js"></script>
     <!-- Slicknav JS -->
-    <script src="public/js/slicknav.min.js"></script>
+    <script src="js/slicknav.min.js"></script>
     <!-- ScrollUp JS -->
-    <script src="public/js/jquery.scrollUp.min.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
     <!-- Niceselect JS -->
-    <script src="public/js/niceselect.js"></script>
+    <script src="js/niceselect.js"></script>
     <!-- Tilt Jquery JS -->
-    <script src="public/js/tilt.jquery.min.js"></script>
+    <script src="js/tilt.jquery.min.js"></script>
     <!-- Owl Carousel JS -->
-    <script src="public/js/owl-carousel.js"></script>
+    <script src="js/owl-carousel.js"></script>
     <!-- counterup JS -->
-    <script src="public/js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
     <!-- Steller JS -->
-    <script src="public/js/steller.js"></script>
+    <script src="js/steller.js"></script>
     <!-- Wow JS -->
-    <script src="public/js/wow.min.js"></script>
+    <script src="js/wow.min.js"></script>
     <!-- Magnific Popup JS -->
-    <script src="public/js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
     <!-- Counter Up CDN JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
     <!-- Bootstrap JS -->
-    <script src="public/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <!-- Main JS -->
-    <script src="public/js/main.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
